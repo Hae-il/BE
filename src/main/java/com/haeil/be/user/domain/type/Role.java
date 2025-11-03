c94dfec3e9d2c466ed9f00e608df3ee8af6ddfb9
erika0915 <soohee2001@gmail.com>
2025-11-03 23:56:30 +0900

    refactor: role 수정 (#1)

--- src/main/java/com/haeil/be/user/domain/type/Role.java
+++ src/main/java/com/haeil/be/user/domain/type/Role.java
@@ -3,7 +3,7 @@ package com.haeil.be.user.domain.type;
 public enum Role {
     ROLE_ATTORNEY,
     ROLE_SECRETARY,
-    ROLE_COUNSEL_ATTORNEY,
-    ROLE_ACCOUNT_TEAM,
+    ROLE_COUNSEL,
+    ROLE_ACCOUNT,
     ROLE_ADMIN,
 }
