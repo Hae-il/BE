7a94a42bd939008a18f0448e62a558c0f09189d2
erika0915 <soohee2001@gmail.com>
2025-11-03 23:57:06 +0900

    feat: 회원가입 기능 구현시 User관련 코드 추가 (#1)

--- src/main/java/com/haeil/be/user/domain/User.java
+++ src/main/java/com/haeil/be/user/domain/User.java
@@ -3,6 +3,7 @@ package com.haeil.be.user.domain;
 import com.haeil.be.global.entity.BaseEntity;
 import com.haeil.be.user.domain.type.Role;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -19,7 +20,21 @@ public class User extends BaseEntity {
     @Column(name="name")
     private String name;
 
+    @Column(name="email")
+    private String email;
+
+    @Column(name="password")
+    private String password;
+
     @Column(name="role")
     @Enumerated(EnumType.STRING)
     private Role role;
+
+    @Builder
+    public User(String name, String email, String password, Role role) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+        this.role = role;
+    }
 }
--- src/main/java/com/haeil/be/user/repository/UserRepository.java
+++ src/main/java/com/haeil/be/user/repository/UserRepository.java
@@ -7,4 +7,6 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findUserById(Long id);
+
+    boolean existsByEmail(String email);
 }
