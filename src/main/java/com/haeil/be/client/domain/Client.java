60e5b1a28711f83fdbb5533fbef736a3e0da2ec1
erika0915 <soohee2001@gmail.com>
2025-11-05 15:34:50 +0900

    feat: client 관련 설정 추가 (#4)

--- src/main/java/com/haeil/be/client/domain/Client.java
+++ src/main/java/com/haeil/be/client/domain/Client.java
@@ -1,4 +1,20 @@
 package com.haeil.be.client.domain;
 
-public class Client {
+import com.haeil.be.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="client")
+@Entity
+@Getter
+@NoArgsConstructor
+public class Client extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name="name")
+    private String name;
 }
--- src/main/java/com/haeil/be/client/service/ClientService.java
+++ src/main/java/com/haeil/be/client/service/ClientService.java
@@ -1,4 +1,9 @@
 package com.haeil.be.client.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ClientService {
 }
