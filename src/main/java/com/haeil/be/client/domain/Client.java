d6cb41d7026fe86b90a43c80d03d72807ebd7010
erika0915 <soohee2001@gmail.com>
2025-11-05 20:48:21 +0900

    feat: 의뢰인 관련 엔티티 작성 (#4)

--- src/main/java/com/haeil/be/client/domain/Client.java
+++ src/main/java/com/haeil/be/client/domain/Client.java
@@ -1,9 +1,13 @@
 package com.haeil.be.client.domain;
 
+import com.haeil.be.consultation.domain.Consultation;
 import com.haeil.be.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Table(name="client")
 @Entity
@@ -17,4 +21,16 @@ public class Client extends BaseEntity {
 
     @Column(name="name")
     private String name;
+
+    @Column(name="email")
+    private String email;
+
+    @Column(name="phone")
+    private String phone;
+
+    @Column(name="address")
+    private String address;
+
+    @OneToMany(mappedBy = "client", fetch = FetchType.LAZY)
+    private List<Consultation> consultations = new ArrayList<>();
 }
