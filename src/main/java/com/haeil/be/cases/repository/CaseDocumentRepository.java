ac5e4e14a58f576e3136cacb00aae889f3e572d6
yjy323 <beachu23@naver.com>
2025-11-21 17:52:36 +0900

    style: 코드 포매팅 및 스타일 정리
    - import 문 순서 정리
    - 주석 스타일 통일 (// 형식으로 변경)
    - 파라미터 줄바꿈 스타일 정리
    - 코드 가독성 향상
    
    #44

--- src/main/java/com/haeil/be/cases/repository/CaseDocumentRepository.java
+++ src/main/java/com/haeil/be/cases/repository/CaseDocumentRepository.java
@@ -7,4 +7,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface CaseDocumentRepository extends JpaRepository<CaseDocument, Long> {
     List<CaseDocument> findByCasesId(Long caseId);
 }
-
--- src/main/java/com/haeil/be/cases/repository/PetitionRepository.java
+++ src/main/java/com/haeil/be/cases/repository/PetitionRepository.java
@@ -3,6 +3,4 @@ package com.haeil.be.cases.repository;
 import com.haeil.be.cases.domain.Petition;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PetitionRepository extends JpaRepository<Petition, Long> {
-}
-
+public interface PetitionRepository extends JpaRepository<Petition, Long> {}
