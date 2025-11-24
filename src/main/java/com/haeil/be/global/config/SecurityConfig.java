9bf56f7e8ad67f25db81f73774d6ebf325d42c39
erika0915 <soohee2001@gmail.com>
2025-11-24 15:29:52 +0900

    feat: SecurityConfig에 수임 관련 ACCOUNT ROLE 만 접근 가능하도록 설정 (#50)

--- src/main/java/com/haeil/be/global/config/SecurityConfig.java
+++ src/main/java/com/haeil/be/global/config/SecurityConfig.java
@@ -51,6 +51,8 @@ public class SecurityConfig {
                                                 "/api/v1/auth/signup",
                                                 "/api/v1/auth/login")
                                         .permitAll()
+                                        .requestMatchers("/api/v1/contract/**")
+                                        .hasRole("ACCOUNT")
                                         .requestMatchers("/api/v1/cases/unassigned/**")
                                         .hasRole("SECRETARY")
                                         .requestMatchers(
