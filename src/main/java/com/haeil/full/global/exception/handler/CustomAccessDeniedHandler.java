e9bf22e7f5b4818060d0bfb69558cde846ce5063
조민성 <dnglzl0823@gmail.com>
2025-12-07 17:21:51 +0900

    feat: 권한이 없는 페이지 접속 시 메시지 출력(#73) - 기존 403 페이지 대신 권한이 없다는 메시지 출력

---
+++ src/main/java/com/haeil/full/global/exception/handler/CustomAccessDeniedHandler.java
@@ -0,0 +1,25 @@
+package com.haeil.full.global.exception.handler;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import org.springframework.security.access.AccessDeniedException;
+import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAccessDeniedHandler implements AccessDeniedHandler {
+
+    @Override
+    public void handle(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            AccessDeniedException accessDeniedException)
+            throws IOException, ServletException {
+
+        response.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        response.setContentType("text/html;charset=UTF-8");
+        response.getWriter().write("<script>alert('권한이 없습니다.'); history.back();</script>");
+    }
+}
